5a28a36705b2211cdf6bc3489d7d77865c4e77f4
salam.mia <salam.mia@selise.ch>
2018-04-24 20:11:31 +0600

    draw railine

--- typescript.js
+++ typescript.js
@@ -1,41 +1,78 @@
 var canvas;
 var ctx;
 var railWayPath = /** @class */ (function () {
-    function railWayPath(x, y, radius, color, linewidth) {
-        if (radius === void 0) { radius = 2; }
+    function railWayPath(a, b, x, y, color, linewidth) {
         if (color === void 0) { color = 'white'; }
-        if (linewidth === void 0) { linewidth = 2; }
+        if (linewidth === void 0) { linewidth = 5; }
         var _this = this;
         this.x = 0;
         this.y = 0;
+        this.a = 0;
+        this.b = 0;
         this.color = "red";
-        this.lineWidth = 2;
-        this.radius = 10;
+        this.lineWidth = 5;
         this.draw = function () {
             ctx.save();
             ctx.beginPath();
             ctx.strokeStyle = _this.color;
             ctx.lineWidth = _this.lineWidth;
-            ctx.arc(_this.x, _this.y, _this.radius, 0, 2 * Math.PI);
+            ctx.moveTo(_this.a, _this.b);
+            ctx.lineTo(_this.x, _this.y);
             ctx.stroke();
-            ctx.restore();
+            ctx.lineCap = 'round';
+        };
+        this.drawVertical = function () {
+            for (var i = 80; i < 1280; i += 80) {
+                ctx.save();
+                ctx.beginPath();
+                ctx.strokeStyle = "#808000";
+                ctx.lineWidth = _this.lineWidth;
+                ctx.moveTo(i, 430);
+                ctx.lineTo(i, 280);
+                ctx.stroke();
+                ctx.lineCap = 'round';
+            }
         };
         this.x = x;
         this.y = y;
+        this.a = a;
+        this.b = b;
         this.color = color;
         this.lineWidth = linewidth;
-        this.radius = radius;
     }
     return railWayPath;
 }());
-var railWayPath1 = new railWayPath(200, 300, 50);
-var railWayPath2 = new railWayPath(400, 550, 150, "blue", 5);
+// for (let i:number = 80; i < 1280; i += 80){
+//     ctx.beginPath();
+//     ctx.strokeStyle = "#808000";
+//     ctx.lineWidth = 5;
+//     ctx.moveTo(i, 430);
+//     ctx.lineTo(i,280);
+//     ctx.stroke();
+// }
+var railWayPath1 = new railWayPath(0, 400, 1280, 400);
+var railWayPath2 = new railWayPath(0, 300, 1280, 300);
+var railWayPath3 = new railWayPath(0, 0, 0, 0);
+// var railWayPath3: railWayPath = new railWayPath( 80, 430, 80, 280, "#808000");
+// var railWayPath4: railWayPath = new railWayPath(160, 430, 160, 280, "#808000");
+// var railWayPath5: railWayPath = new railWayPath(240, 430, 240, 280, "#808000");
+// var railWayPath6: railWayPath = new railWayPath(320, 430, 320, 280,  "#808000");
+// var railWayPath7: railWayPath = new railWayPath(400, 430, 400, 280, "#808000");
+// var railWayPath8: railWayPath = new railWayPath(480, 430, 480, 280, "#808000");
+// var railWayPath9: railWayPath = new railWayPath(560, 430, 560, 280,  "#808000");
 function gameLoop() {
     requestAnimationFrame(gameLoop);
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, 1280, 720);
     railWayPath1.draw();
     railWayPath2.draw();
+    railWayPath3.drawVertical();
+    //    railWayPath4.draw();
+    //    railWayPath5.draw();
+    //    railWayPath6.draw();
+    //    railWayPath7.draw();
+    //    railWayPath8.draw();
+    //    railWayPath9.draw();
 }
 window.onload = function () {
     canvas = document.getElementById('cnvs');
--- typescript.ts
+++ typescript.ts
@@ -4,24 +4,30 @@ let ctx: CanvasRenderingContext2D;
 interface iShape {
    x: number;
    y: number;
+   a:number;
+   b:number;
    color: string;
    lineWidth: number;
    draw(): void;
+   drawVertical(): void;
 }
 
 class railWayPath implements iShape{
   public x: number = 0;
   public y: number = 0;
+  public a: number = 0;
+  public b: number = 0;
   public color: string = "red";
-  public lineWidth: number = 2;
-  public radius: number = 10;
-  
-  constructor(x:number, y:number, radius:number = 2, color:string = 'white', linewidth:number = 2 ){
-      this.x=x;
-      this.y=y;
-      this.color=color;
+  public lineWidth: number = 5;
+
+  constructor(a:number, b:number, x:number, y:number,  color:string = 'white', linewidth:number = 5 ){
+      this.x = x;
+      this.y = y;
+      this.a = a;
+      this.b = b;
+      this.color = color;
       this.lineWidth = linewidth;
-      this.radius = radius;
+    
   }
 
   public draw = ():void =>{
@@ -29,22 +35,56 @@ class railWayPath implements iShape{
       ctx.beginPath();
       ctx.strokeStyle = this.color;
       ctx.lineWidth = this.lineWidth;
-      ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+      ctx.moveTo(this.a, this.b);
+      ctx.lineTo(this.x, this.y);
       ctx.stroke();
-      ctx.restore();
+     ctx.lineCap = 'round';
+    
+  }
+  public drawVertical = ():void =>{
+      for (let i:number = 80; i < 1280; i += 80){
+        ctx.save();
+        ctx.beginPath();
+        ctx.strokeStyle = "#808000";
+        ctx.lineWidth = this.lineWidth;
+        ctx.moveTo(i, 430);
+        ctx.lineTo(i, 280);
+        ctx.stroke();
+        ctx.lineCap = 'round';
+      }
+    
   }
 }
 
+var railWayPath1: railWayPath = new railWayPath(0, 400, 1280, 400 );
+var railWayPath2: railWayPath = new railWayPath( 0, 300, 1280, 300);
+var railWayPath3: railWayPath = new railWayPath( 0, 0, 0, 0);
 
-var railWayPath1: railWayPath = new railWayPath(200, 300, 50);
-var railWayPath2: railWayPath = new railWayPath(400, 550, 150, "blue", 5);
+
+// var railWayPath3: railWayPath = new railWayPath( 80, 430, 80, 280, "#808000");
+// var railWayPath4: railWayPath = new railWayPath(160, 430, 160, 280, "#808000");
+// var railWayPath5: railWayPath = new railWayPath(240, 430, 240, 280, "#808000");
+// var railWayPath6: railWayPath = new railWayPath(320, 430, 320, 280,  "#808000");
+// var railWayPath7: railWayPath = new railWayPath(400, 430, 400, 280, "#808000");
+// var railWayPath8: railWayPath = new railWayPath(480, 430, 480, 280, "#808000");
+// var railWayPath9: railWayPath = new railWayPath(560, 430, 560, 280,  "#808000");
+
 
 function gameLoop() {
    requestAnimationFrame(gameLoop);
    ctx.fillStyle = "black";
    ctx.fillRect(0, 0, 1280, 720);
+
    railWayPath1.draw();
    railWayPath2.draw();
+   railWayPath3.drawVertical();
+//    railWayPath4.draw();
+//    railWayPath5.draw();
+//    railWayPath6.draw();
+//    railWayPath7.draw();
+//    railWayPath8.draw();
+//    railWayPath9.draw();
+
 }
 
 window.onload = ():void => {
